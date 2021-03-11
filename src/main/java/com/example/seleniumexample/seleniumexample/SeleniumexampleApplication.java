b4a99ccf336efa3fd4c2ca6d1ce7073d5fdbe7aa
siddheshmoharir <80397543+siddheshmoharir@users.noreply.github.com>
2021-03-11 12:27:16 +0530

    Added import for scrolling

--- src/main/java/com/example/seleniumexample/seleniumexample/SeleniumexampleApplication.java
+++ src/main/java/com/example/seleniumexample/seleniumexample/SeleniumexampleApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+
 
 import java.awt.*;
 import java.net.MalformedURLException;
